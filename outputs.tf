2bdb7f2028cfd6aeb3c9ad19fdfe7cafa9b23c42
Luís Alves <24353152+alves-luis@users.noreply.github.com.>
2025-07-23 11:51:15 +0100

    Add headscale instance IP

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "bucket_region" {
   value = scaleway_object_bucket.truenas_backup.region
   description = "The region where the TrueNAS backup bucket is located."
 }
+
+output "headscale_ips" {
+  value = scaleway_instance_server.headscale.public_ips[*].address
+  description = "Headscale public IP addresses."
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "organization_id" {
   type        = string
   description = "Scaleway organization ID."
 }
+
+variable "public_ssh_key" {
+  type        = string
+  description = "Public SSH key to be used for authentication."
+}
